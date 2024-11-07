a3e5e7f1229c1bbbbf62e6decdbe75fcbadd7be6
fhumulani mulaudzi <115710782+fhumulani12@users.noreply.github.com>
2024-11-07 12:22:25 +0200

    feat(cv): upload new cv

--- src/Components/Homehero.tsx
+++ src/Components/Homehero.tsx
@@ -52,7 +52,7 @@ const Homehero = () => {
             </Fade>
             <Fade direction="right">
               <a
-                href=" https://drive.google.com/file/d/1_pyN9WIEin5RAisqBVKBR9lchf1Qb1B5/view?usp=drive_link "
+                href=" https://drive.google.com/file/d/1GTwL0VZytJDwk6kakH64Y403XS883y0d/view?usp=sharing"
                 target="_blank"
               >
                 <button className="border rounded-lg font-semibold border-black px-2 py-3  transition duration-300 ease-in-out hover:bg-[#4267B2] hover:border-none hover:text-white ">
